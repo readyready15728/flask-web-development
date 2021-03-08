47a1956a5404115d209199216bc15b3d68cf97c6
Lynn Bradshaw <readyready15728@gmail.com>
2021-03-08 01:53:21 +0000

    Adding project make-response

---
+++ 02/make-response/make_response.py
@@ -0,0 +1,8 @@
+from flask import Flask, make_response
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    response = make_response('<h1>This document carries a cookie!')
+    response.set_cookie('answer', '42')
+    return response
