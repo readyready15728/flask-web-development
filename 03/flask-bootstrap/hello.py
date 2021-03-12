c0c52425176e74f8ac27842c9eeecaeba19aded4
Lynn Bradshaw <readyready15728@gmail.com>
2021-03-12 20:35:42 +0000

    Renaming 500 error handlers proper

--- 03/flask-bootstrap/hello.py
+++ 03/flask-bootstrap/hello.py
@@ -17,5 +17,5 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def page_not_found(e):
+def internal_server_error(e):
     return render_template('500.html'), 500
